036db2fa5a8f90ee45cfe68827328829921539fc
Alessio Franchini <130763379+FrAle01@users.noreply.github.com>
2024-09-26 12:47:22 +0200

    Delete coap-actuator/build directory

--- coap-actuator/build/cooja/obj/.deps/queue_manager.c.d
+++
@@ -1,4 +0,0 @@
-build/cooja/obj/queue_manager.o: utils/queue_manager.c \
- ../../../os/net/app-layer/coap/module-macros.h utils/queue_manager.h
-../../../os/net/app-layer/coap/module-macros.h:
-utils/queue_manager.h:
--- coap-actuator/build/nrf52840/dongle/obj/.deps/queue_manager.c.d
+++
@@ -1,6 +0,0 @@
-build/nrf52840/dongle/obj/queue_manager.o: utils/queue_manager.c \
- ../../../os/net/app-layer/coap/module-macros.h utils/queue_manager.h
-
-../../../os/net/app-layer/coap/module-macros.h:
-
-utils/queue_manager.h:
