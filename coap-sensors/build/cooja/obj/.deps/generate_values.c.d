b321fc36cce458d6e87bbeec395b9806edc61944
Alessio Franchini <130763379+FrAle01@users.noreply.github.com>
2024-09-26 12:46:43 +0200

    Delete coap-sensors/build/cooja directory

--- coap-sensors/build/cooja/obj/.deps/generate_values.c.d
+++
@@ -1,4 +0,0 @@
-build/cooja/obj/generate_values.o: utils/generate_values.c \
- ../../../os/net/app-layer/coap/module-macros.h utils/generate_values.h
-../../../os/net/app-layer/coap/module-macros.h:
-utils/generate_values.h:
--- coap-sensors/build/cooja/obj/.deps/timestamp.c.d
+++
@@ -1,4 +0,0 @@
-build/cooja/obj/timestamp.o: utils/timestamp.c \
- ../../../os/net/app-layer/coap/module-macros.h utils/timestamp.h
-../../../os/net/app-layer/coap/module-macros.h:
-utils/timestamp.h:
