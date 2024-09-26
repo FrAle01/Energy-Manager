bf3c47f3d098d03d84d8456a7dade11fa0b3c3fb
Alessio Franchini <130763379+FrAle01@users.noreply.github.com>
2024-09-26 12:46:25 +0200

    Delete coap-actuator/build/cooja directory

--- coap-actuator/build/cooja/obj/.deps/queue_manager.c.d
+++
@@ -1,6 +0,0 @@
-build/cooja/obj/queue_manager.o: utils/queue_manager.c \
- ../../../os/net/app-layer/coap/module-macros.h utils/queue_manager.h \
- utils/timestamp_module.h
-../../../os/net/app-layer/coap/module-macros.h:
-utils/queue_manager.h:
-utils/timestamp_module.h:
--- coap-actuator/build/cooja/obj/.deps/timestamp_module.c.d
+++
@@ -1,4 +0,0 @@
-build/cooja/obj/timestamp_module.o: utils/timestamp_module.c \
- ../../../os/net/app-layer/coap/module-macros.h utils/timestamp_module.h
-../../../os/net/app-layer/coap/module-macros.h:
-utils/timestamp_module.h:
