ff303912b69fbe74f60452c1a59022ac0a99c3ff
Alessio Franchini <130763379+FrAle01@users.noreply.github.com>
2024-09-26 12:47:42 +0200

    Delete coap-sensors/build directory

--- coap-sensors/build/nrf52840/dongle/obj/.deps/generate_values.c.d
+++
@@ -1,6 +0,0 @@
-build/nrf52840/dongle/obj/generate_values.o: utils/generate_values.c \
- ../../../os/net/app-layer/coap/module-macros.h utils/generate_values.h
-
-../../../os/net/app-layer/coap/module-macros.h:
-
-utils/generate_values.h:
--- coap-sensors/build/nrf52840/dongle/obj/.deps/timestamp.c.d
+++
@@ -1,6 +0,0 @@
-build/nrf52840/dongle/obj/timestamp.o: utils/timestamp.c \
- ../../../os/net/app-layer/coap/module-macros.h utils/timestamp.h
-
-../../../os/net/app-layer/coap/module-macros.h:
-
-utils/timestamp.h:
